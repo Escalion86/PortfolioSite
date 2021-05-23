1d6c3c463356537be4e6e985e702ab9f70ca3fc8
Aleksei Belinskiy <escalion86@gmail.com>
2021-05-24 02:49:57 +0700

    Try to fix <title>

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -10,7 +10,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Pattaya&display=swap"
             rel="stylesheet"
           />
-          <title>Разработчик сайтов и приложений Алексей Белинский</title>
           <meta
             name="description"
             content="Профессиональный FullStack разработчик сайтов. Разработка от лендингов, до крупных проектов с базами данных и административной панелью."
@@ -19,6 +18,7 @@ class MyDocument extends Document {
             name="keywords"
             content="Программист, Разработчик сайтов, Разработка сайтов, Разработчик приложений, Разработчик приложения, Разработка приложения, Разработка приложений, Fullstack developer, developer"
           />
+          <title>Разработчик сайтов и приложений Алексей Белинский</title>
         </Head>
         <body className="bg-gradient-to-r from-primary to-secondary dark:from-dark-500 dark:to-dark-700 dark:text-white">
           <Main />
